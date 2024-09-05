919629f89374664630ff98163f7d7c847f55a7da
Erin Cameron <erincameron@Lauras-MacBook-Pro.local>
2024-09-05 17:24:11 -0400

    transfer data to parquet files format

--- SurvivalAnalysisTool.py
+++ SurvivalAnalysisTool.py
@@ -8,14 +8,19 @@ import kaplanmeier as km
 # Cache the dataframe using st.cache_data decorator
 @st.cache_data
 # Function to read in the RNA & ID/Gene mapping tsv files
-def load_data(filename, mapping_filename):
+def load_data(rna_filename, gene_mapping_filename, survival_filename, phenotype_filename):
     # RNA matrix 
-    df = pd.read_csv(filename, sep='\t')
+    # df = pd.read_csv(filename, sep='\t')
+    df = pd.read_parquet(rna_filename)
     # ID/Gene Mapping file
-    mapping = pd.read_csv(mapping_filename, sep='\t')
+    mapping = pd.read_csv(gene_mapping_filename, sep='\t')
     mapping.rename(columns={'id': 'xena_sample'}, inplace=True) # rename column to merge on
     merged_df = pd.merge(df, mapping, on='xena_sample', how='left')
-    return merged_df
+
+    survival_df = pd.read_parquet(survival_filename)
+    phenotype_df = pd.read_parquet(phenotype_filename)
+    
+    return merged_df, survival_df, phenotype_df
 
 
 # ------------------ HELPER FUNCTIONS ------------------
@@ -75,7 +80,24 @@ def customize_submit_button(initial_bg_color: str, initial_outline_color: str, i
     """
     st.markdown(css, unsafe_allow_html=True)
 
+# Function to create a downloadable csv of GSVA data
+@st.cache_data
+def convert_gsva_df(df):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    return df.to_csv().encode("utf-8")
 
+# Function to calculate GSVA scores
+def calculate_gsva():
+    if genes_entered:
+        # TODO: calculate the gsva
+        print("calculating the gsva")
+    # Genes need to be entered first
+    else:
+        # TODO: display an error message and clear the dropdown selection
+        st.write("ENTER GENE NAMES FIRST PLEASE")
+        
+
+    
 # ------------------ APP ------------------
 def main():
     # App title
@@ -86,16 +108,46 @@ def main():
     st.write("Informational text area")
 
     # Call the load data method
-    df = load_data('./data/GDC-PANCAN.htseq_fpkm-uq.tsv', './data/gencode.v22.annotation.gene.probeMap')
+    df, survival_df, phenotype_df = load_data('./data/GDC-PANCAN.htseq_fpkm-uq.parquet', 
+                                              './data/gencode.v22.annotation.gene.probeMap',
+                                              './data/GDC-PANCAN.survival.parquet', 
+                                              './data/GDC-PANCAN.basic_phenotype.parquet')
+    # df = load_data('./data/GDC-PANCAN.htseq_fpkm-uq.tsv', './data/gencode.v22.annotation.gene.probeMap')
     
     # Locate all gene names in a list
     gene_names = df['gene'].unique()
     
     # Multiselect element for gene selection
     genes_entered = st.multiselect(
-        "Enter Genes:",
+        "Gene Names:",
         gene_names,
+        placeholder="Enter gene names",
     )
+
+    # Dropdown for cancer type
+    cancer_types = phenotype_df['project_id'].unique()
+    cancer_type_entered = st.multiselect(
+        "Cancer Type:",
+        (cancer_types),
+        placeholder="Select cancer type",
+        on_change=calculate_gsva,
+    )
+
+    # # TODO: Calculate GSVA scores
+    # d = {'col1': [1, 2], 'col2': [3, 4]}
+    # df_gsva = pd.DataFrame(data=d)
+    
+    # # Download button for GSVA output
+    # csv = convert_gsva_df(df_gsva)
+    # st.download_button(
+    #     label="Download GSVA data as CSV",
+    #     data=csv,
+    #     file_name="gsva.csv",
+    #     mime="text/csv",
+    # )
+
+    # # Dropdown for cutoff...
+    
 
     # Apply the CSS to change the color of the multiselect tags etc.
     change_multiselect_colours("#4A9661", "gray")
